d14431022ec0a3845f5f15a7ba4821ccbcb5957f
Yamagata Fumihiro <bun@kushiro-ct.ac.jp>
2013-07-25 01:19:24 +0900

    Add License header

--- RINEX_AllTests/stdafx.h
+++ RINEX_AllTests/stdafx.h
@@ -1,3 +1,13 @@
+//----------------------------------------------------------------------
+//Copyright (c) 2013 Yamagata Fumihiro.
+//All rights reserved. This program and the accompanying materials
+//are made available under the terms of the GNU Public License v2.0
+//which accompanies this distribution, and is available at
+//http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+//
+//Contributors:
+//    Yamagata Fumihiro - initial API and implementation
+//----------------------------------------------------------------------
 // stdafx.h : 標準のシステム インクルード ファイルのインクルード ファイル、または
 // 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード ファイル
 // を記述します。
--- RINEXanalyzer/main.cpp
+++ RINEXanalyzer/main.cpp
@@ -4,6 +4,16 @@
  *  Created on: 2013/06/08
  *      Author: bun
  */
+// ----------------------------------------------------------------------
+// Copyright (c) 2013 Yamagata Fumihiro.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the GNU Public License v2.0
+// which accompanies this distribution, and is available at
+// http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+// 
+// Contributors:
+//     Yamagata Fumihiro - initial API and implementation
+// ----------------------------------------------------------------------
 
 #include "stdafx.h"
 
